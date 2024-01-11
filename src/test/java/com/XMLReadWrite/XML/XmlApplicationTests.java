dc9f7162c8bac26e5bf6958416cd5c2dd71d7092
darshan-parsai <darshanparsai@gmail.com>
2024-01-11 16:19:01 +0530

    First commit

---
+++ src/test/java/com/XMLReadWrite/XML/XmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.XMLReadWrite.XML;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
